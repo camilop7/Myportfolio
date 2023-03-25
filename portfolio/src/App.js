8ee94b7863fc1a553869492b17107d67e4ffa292
Camilo Palacios <camilop7@icloud.com>
2023-03-25 11:55:01 +0000

    first commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Camilo Andres Palacios
+        </p>
+          <p>
+          Software development
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
